2ffe1d5be2c4a2339b583403d68d8f8e8cf67bae
Scantraxx123 <34422209+Scantraxx123@users.noreply.github.com>
2018-12-11 18:22:05 +0100

    First Changes, Rekursion fehlt noch bei getKanidat und das Speichern im neuen Objekt

--- main.hs
+++ main.hs
@@ -7,6 +7,30 @@ data Voter = Voter {
   vote :: [(String,Double)]
 } deriving (Show)
 
+
+
+kanidat1 = Wahlobjekt "kanidat1" 0
+kanidat2 = Wahlobjekt "kanidat2" 0
+listeKanidaten = [kanidat1,kanidat2]
+
+voter1=Voter [("kanidat1",3),("kanidat1",2)]
+voter2=Voter [("kanidat1",1),("kanidat2",4)]
+listeVoter = [voter1,voter2]
+
+
+--getVoter[] = []
+--getVoter(x:xs) ys = getKanidat x ys
+getKanidat x [] = 0
+getKanidat x (y:ys)
+ | fst(head(vote x)) == (bezeichnung y) =  add x y
+ | otherwise = getKanidat x ys
+
+
+add x y = snd(head(vote x)) + (wahlErgebnis y)
+
+
+
+
 -- fst(head(vote vote1)) um ersten Eintrag vom Tumpel zu bekommen
 -- snd(head(vote vote1)) um zweiten Eintrag vom Tumpel zu bekommen
 
