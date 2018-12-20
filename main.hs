82b618656620b36ea175b8e9aec2a759805b9f68
Scantraxx123 <34422209+Scantraxx123@users.noreply.github.com>
2018-12-20 21:29:23 +0100

    Auf English alles umbennant

--- main.hs
+++ main.hs
@@ -1,21 +1,21 @@
 import Data.Function (on)
 import Data.List (sortBy)
 
-data Wahlobjekt = Wahlobjekt {
-    bezeichnung   :: String,
-    wahlErgebnis  :: Double
+data VoteObject = VoteObject {
+    name   :: String,
+    voteResult  :: Double
   } deriving (Show)
 
 data Voter = Voter {
   vote :: [(String,Double)]
 } deriving (Show)
 
-kandidat1 = Wahlobjekt "kandidat1" 0
-kandidat2 = Wahlobjekt "kandidat2" 0
-kandidat3 = Wahlobjekt "kandidat3" 0
-kandidat4 = Wahlobjekt "kandidat4" 0
-kandidat5 = Wahlobjekt "kandidat5" 0
-kandidat6 = Wahlobjekt "kandidat6" 0
+kandidat1 = VoteObject "kandidat1" 0
+kandidat2 = VoteObject "kandidat2" 0
+kandidat3 = VoteObject "kandidat3" 0
+kandidat4 = VoteObject "kandidat4" 0
+kandidat5 = VoteObject "kandidat5" 0
+kandidat6 = VoteObject "kandidat6" 0
 listeKandidaten = [kandidat1,kandidat2,kandidat3,kandidat4,kandidat5,kandidat6]
 
 voter1=Voter [("kandidat1",1),("kandidat2",3),("kandidat3",2),("kandidat4",3),("kandidat5",4),("kandidat6",0)]
@@ -27,34 +27,34 @@ voter6=Voter [("kandidat1",1),("kandidat2",1),("kandidat3",1),("kandidat4",1),("
 listeVoter = [voter1,voter2,voter3,voter4,voter5,voter6]
 
 
-getResults :: [Wahlobjekt] -> [Voter] -> [Wahlobjekt]
+getResults :: [VoteObject] -> [Voter] -> [VoteObject]
 getResults (x:xs) voters
         | null (x:xs) = []
-        | length (x:xs) == 1 = (Wahlobjekt (bezeichnung x) (getRating voters x)) : []
-        | otherwise = (Wahlobjekt (bezeichnung x) (getRating voters x)) : getResults xs voters
+        | length (x:xs) == 1 = (VoteObject (name x) (getRating voters x)) : []
+        | otherwise = (VoteObject (name x) (getRating voters x)) : getResults xs voters
 
-getRating :: [Voter] -> Wahlobjekt -> Double
+getRating :: [Voter] -> VoteObject -> Double
 getRating (x:xs) kandidat
         | null (x:xs) = 0
-        | length (x:xs) == 1 = if (fst(head(vote (filteredVoter x kandidat))) == (bezeichnung kandidat)) then snd(head(vote (filteredVoter x kandidat))) else 0
-        | fst(head(vote (filteredVoter x kandidat))) == (bezeichnung kandidat) = snd(head(vote (filteredVoter x kandidat))) + (getRating xs kandidat)
+        | length (x:xs) == 1 = if (fst(head(vote (filteredVoter x kandidat))) == (name kandidat)) then snd(head(vote (filteredVoter x kandidat))) else 0
+        | fst(head(vote (filteredVoter x kandidat))) == (name kandidat) = snd(head(vote (filteredVoter x kandidat))) + (getRating xs kandidat)
         | otherwise = getRating xs kandidat
 
-filteredVoter :: Voter -> Wahlobjekt -> Voter
-filteredVoter voter wahlobjekt = Voter (filter ((==bezeichnung wahlobjekt).fst) (vote voter))
+filteredVoter :: Voter -> VoteObject -> Voter
+filteredVoter voter voteObject = Voter (filter ((==name voteObject).fst) (vote voter))
 
 
-getRunoffCanidates :: [Wahlobjekt] -> [Wahlobjekt]
+getRunoffCanidates :: [VoteObject] -> [VoteObject]
 getRunoffCanidates xs
         | null xs = []
         | length(xs) <= 2 = xs
-        | otherwise = checkIfSameScore otherCanidates runoffCanidates--drop (length sorted - 2) sorted
+        | otherwise = checkIfSameScore otherCanidates runoffCanidates
           where
-            sorted = sortBy (compare `on` wahlErgebnis) xs
+            sorted = sortBy (compare `on` voteResult) xs
             otherCanidates = take (length sorted - 2) sorted
             runoffCanidates = drop (length sorted - 2) sorted
 
-checkIfSameScore :: [Wahlobjekt] -> [Wahlobjekt] -> [Wahlobjekt]
+checkIfSameScore :: [VoteObject] -> [VoteObject] -> [VoteObject]
 checkIfSameScore [] ys = ys
 checkIfSameScore xs [] = []
 checkIfSameScore xs (y:ys)
@@ -62,8 +62,8 @@ checkIfSameScore xs (y:ys)
       | newCanidateResult == runoffCanidateResult = checkIfSameScore (init xs) ((last xs):(y:ys))
       | otherwise = (y:ys)
         where
-          newCanidateResult = wahlErgebnis (last xs)
-          runoffCanidateResult = wahlErgebnis y
+          newCanidateResult = voteResult (last xs)
+          runoffCanidateResult = voteResult y
 
 
 
