ca833cd507c3f21a7ce9d22cc004bd59b7662142
Scantraxx123 <34422209+Scantraxx123@users.noreply.github.com>
2019-01-21 15:17:28 +0100

    Skript zur Erstellung von XML Dateien erstellen

---
+++ Skript_XML.py
@@ -0,0 +1,20 @@
+import random
+
+durchlauefe = 10000
+kandidaten = ["Marty","Peter","Dieter","Klaus"]
+string_file = ""
+
+
+text_file = open("Output.xml", "w")
+string_file += "<VOTERS>\n"
+for x in range(0,durchlauefe):
+    string_file += "<VOTER>\n"
+    for y in range(0,len(kandidaten)):   
+        rnd = random.randint(0,5)
+        string_file += "<VOTE CANDIDATE=\""+kandidaten[y]+"\" RATING=\""+str(rnd)+"\"></VOTE>\n"
+    string_file += "</VOTER>\n"
+string_file += "</VOTERS>\n"
+
+
+text_file.write(string_file)
+text_file.close()
